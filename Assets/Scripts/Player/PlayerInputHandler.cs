2be6e746080b4f8e1ef236a0037e6e5d45091387
Lukas BEDEN <147034100+Mishaloupe@users.noreply.github.com>
2025-10-20 13:33:43 +0200

    [AssetStore]

---
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    [SerializeField] public InputActionsPlayer _playerInput;
+    private Vector2 _moveInput;
+    private Vector2 _moveCamera;
+    private bool _jumping;
+    private bool _shooting;
+
+    public Vector2 MoveInput => _moveInput;
+    public Vector2 CameraMove => _moveCamera;
+    public bool Jumping => _jumping;
+    public bool Shooting => _shooting;
+
+    private void Awake()
+    {
+        _playerInput = new InputActionsPlayer();
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        _moveInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext ctx)
+    {
+        _moveCamera = ctx.ReadValue<Vector2>();
+    }
+}

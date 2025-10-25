4e84c340703db4144d37ac49415d01b929124b0c
Mishaloupe <147034100+Mishaloupe@users.noreply.github.com>
2025-10-25 16:47:36 +0200

    [FIX] levelSelector duplicate

--- Assets/Scripts/OpenLevelSelector.cs
+++ Assets/Scripts/OpenLevelSelector.cs
@@ -11,7 +11,22 @@ public class OpenLevelSelector : MonoBehaviour
     {
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
-            loadSceneManager.loadScene(sceneName, loadMode);
+            // Vérifie si la scène n'est PAS déjà chargée avant de la recharger
+            if (!IsSceneLoaded(sceneName))
+            {
+                loadSceneManager.loadScene(sceneName, loadMode);
+            }
         }
+    }
+
+    private bool IsSceneLoaded(string name)
+    {
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene scene = SceneManager.GetSceneAt(i);
+            if (scene.name == name)
+                return true;
+        }
+        return false;
     }
 }
